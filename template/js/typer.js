d8f25aa9c9e045e2f6398ca0c9b97c4772ca279d
seun <darealseun@gmail.com>
2018-02-25 21:38:13 +0100

    initial template layout commit

---
+++ template/js/typer.js
@@ -0,0 +1,10 @@
+
+let options = {
+    strings: ["This is the Future!","Welcom to WeConnect","Built to change Businesses","Sign up and up your game"],
+    typeSpeed: 100,
+    loop: true,
+    backSpeed: 10,
+    showCursor: false
+}
+  
+let typed = new Typed(".cover-heading", options);

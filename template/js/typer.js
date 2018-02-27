ec8a5448de84223a3ed2db34103bddd7a5fdc9fc
seun <darealseun@gmail.com>
2018-02-27 16:42:17 +0100

    fix body styles

--- template/js/typer.js
+++ template/js/typer.js
@@ -1,9 +1,9 @@
 
 let options = {
-    strings: ["This is the Future!","Welcom to WeConnect","Built to change Businesses","Sign up and up your game"],
+    strings: ["This is the Future!", "Welcome to WeConnect","Built to change Businesses","Sign up and up your game"],
     typeSpeed: 100,
     loop: true,
-    backSpeed: 10,
+    backSpeed: 6,
     showCursor: false
 }
   

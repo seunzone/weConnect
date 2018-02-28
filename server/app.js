4eab96aeeb86b99626869c919fb2805b05bfd3c9
seun <darealseun@gmail.com>
2018-02-28 10:13:44 +0100

    Basic server-api setup :wrench: :wrench:

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import volleyball from 'volleyball';
+import bodyParser from 'body-parser';
+
+// Defining the Port Variable
+const port = process.env.PORT || 3000;
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(volleyball);
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+    message: 'Welcome to my cool API',
+}));
+  
+app.listen(port);
+  
+export default app;

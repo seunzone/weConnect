1d1f4a753e08f2385871ef4df588177e03578952
seun <darealseun@gmail.com>
2018-03-16 10:43:10 +0100

    Add feature to update business

--- server/controllers/businessController.js
+++ server/controllers/businessController.js
@@ -61,4 +61,56 @@ export default class businessController {
         message: 'Internal server error'
       }));
   }
+  /**
+   * @description - Update a business
+   * @static
+   *
+   * @param {object} req - HTTP Request
+   * @param {object} res - HTTP Response
+   *
+   * @memberof businessController
+   *
+   * @returns {object} Class instance
+   */
+  static updateProfile(req, res) {
+    const {
+      name, description, image, location, category
+    } = req.body;
+
+    Profile.findOne({
+      where: {
+        id: req.params.profileId,
+        userId: req.userId
+      }
+    })
+      .then((foundProfile) => {
+        if (foundProfile) {
+          const update = {
+            name: name || foundProfile.name,
+            description: description || foundProfile.description,
+            image: image || foundProfile.image,
+            location: location || foundProfile.location,
+            category: category || foundProfile.category
+          };
+          foundProfile.update(update)
+            .then(updatedProfile => res.status(200)
+              .json({
+                status: 'success',
+                message: 'Update successful',
+                recipe: updatedProfile
+              }));
+        }
+        if (!foundProfile) {
+          return res.status(404)
+            .json({
+              status: 'fail',
+              message: `Can't find profile with id ${req.params.profileId} by you`
+            });
+        }
+      })
+      .catch(() => res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+      }));
+  }
 }

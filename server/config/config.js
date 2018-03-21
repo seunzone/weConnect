245c6a7cdf0377e361f91e6bb5c1700f0a9147d7
seun <darealseun@gmail.com>
2018-03-21 14:01:48 +0100

    Refactor test db

--- server/config/config.js
+++ server/config/config.js
@@ -9,11 +9,7 @@ const config = {
     dialect: 'postgres'
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_TEST,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+    use_env_variable: 'DB_DATABASE_TEST'
   },
   production: {
     use_env_variable: 'DATABASE_URL',

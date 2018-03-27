4e02582ccb4b5f4b66ae53a30525a1dba9e3f936
seun <darealseun@gmail.com>
2018-03-27 19:08:45 +0100

    Switch db to elephant SQL

--- server/config/config.js
+++ server/config/config.js
@@ -9,11 +9,7 @@ const config = {
     dialect: 'postgres'
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+    use_env_variable: 'DB_TEST_URL'
   },
   production: {
     use_env_variable: 'DATABASE_URL'

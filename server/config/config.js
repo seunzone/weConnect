9aebfee5b3f051cc46b5e4bb815cd15bec9ef353
seun <darealseun@gmail.com>
2018-03-27 17:53:52 +0100

    Fix travis test db

--- server/config/config.js
+++ server/config/config.js
@@ -9,10 +9,11 @@ const config = {
     dialect: 'postgres'
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: 'postgres',
+    password: '',
+    database: 'wecon',
+    host: '127.0.0.1',
+    port: 5432,
     dialect: 'postgres'
   },
   production: {

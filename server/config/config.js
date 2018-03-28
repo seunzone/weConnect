03f3c03f918b1b6a8fd5b9aa6ef5c225df9784de
seun <darealseun@gmail.com>
2018-03-28 11:56:52 +0100

    Set travis db

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,11 @@ const config = {
     dialect: 'postgres'
   },
   test: {
-    use_env_variable: 'DB_TEST_URL'
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
   },
   production: {
     use_env_variable: 'DATABASE_URL'

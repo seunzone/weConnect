13d7da39c76b573c854c1f5160fb6772d9e49732
seun <darealseun@gmail.com>
2018-04-23 11:03:34 +0100

    Render Edit Business Page

--- client/components/main/Routes.jsx
+++ client/components/main/Routes.jsx
@@ -5,14 +5,16 @@ import NotFound from '../common/NotFound';
 import Business from '../businesses/Business';
 import SingleBusiness from '../businesses/SingleBusiness';
 import AddNewBusiness from '../businesses/AddBusiness';
+import EditBusiness from '../businesses/EditBusiness';
 
 export const Main = () => (
     <div>
       <Switch>
-        <Route exact path="/" component={ AddNewBusiness } />
+        <Route exact path="/" component={ EditBusiness } />
         {/* <Route path="/business" exact component={ Business } />
         <Route path="/business/view/:id" exact component={ SingleBusiness } />
         <Route path="/businesses/add" exact component={ AddNewBusiness } />
+        <Route path="/businesses/edit/:id" exact component={ EditBusiness } />
         <Route component={NotFound} /> */}
       </Switch>
     </div>

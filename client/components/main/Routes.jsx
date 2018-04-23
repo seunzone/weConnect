b12bbbee17a62b373847ac4dfb58e935c38a2006
seun <darealseun@gmail.com>
2018-04-23 10:50:33 +0100

    REnder NEw Business PAge

--- client/components/main/Routes.jsx
+++ client/components/main/Routes.jsx
@@ -4,13 +4,15 @@ import ConnectHome  from './Home';
 import NotFound from '../common/NotFound';
 import Business from '../businesses/Business';
 import SingleBusiness from '../businesses/SingleBusiness';
+import AddNewBusiness from '../businesses/AddBusiness';
 
 export const Main = () => (
     <div>
       <Switch>
-        <Route exact path="/" component={ SingleBusiness } />
+        <Route exact path="/" component={ AddNewBusiness } />
         {/* <Route path="/business" exact component={ Business } />
         <Route path="/business/view/:id" exact component={ SingleBusiness } />
+        <Route path="/businesses/add" exact component={ AddNewBusiness } />
         <Route component={NotFound} /> */}
       </Switch>
     </div>

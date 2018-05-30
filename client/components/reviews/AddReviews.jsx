f09bfdc3e1ed5ef089447befc9662b4c6992cdfc
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-4.local>
2018-05-30 11:28:25 +0100

    show user star rating

--- client/components/reviews/AddReviews.jsx
+++ client/components/reviews/AddReviews.jsx
@@ -52,19 +52,19 @@ class AddReviews extends React.Component {
           onChange={this.onChange}
         >
           <input type="radio" id="star5" name="rating" value={5} />
-          <label for="star5" />
+          <label htmlFor="star5" />
 
           <input type="radio" id="star4" name="rating" value={4} />
-          <label for="star4" />
+          <label htmlFor="star4" />
 
           <input type="radio" id="star3" name="rating" value={3} />
-          <label for="star3" />
+          <label htmlFor="star3" />
 
           <input type="radio" id="star2" name="rating" value={2} />
-          <label for="star2" />
+          <label htmlFor="star2" />
 
           <input type="radio" id="star1" name="rating" value={1} />
-          <label for="star1" />
+          <label htmlFor="star1" />
         </fieldset>
         <textarea
           value={this.state.content}

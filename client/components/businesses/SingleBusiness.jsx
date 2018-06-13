44adce515dd25d3848be70c159bdb72a98feade6
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-44.local>
2018-06-13 16:51:44 +0100

    add social media share

--- client/components/businesses/SingleBusiness.jsx
+++ client/components/businesses/SingleBusiness.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import moment from "moment";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+  GooglePlusIcon,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  LinkedinIcon
+} from 'react-share';
 import rating from "../../utils/rating"
 import { addFlashMessage } from "../../actions/flashMessages";
+
 
 //import components
 import AddReviews from "../reviews/AddReviews";
@@ -41,7 +54,7 @@ class SingleBusiness extends React.Component {
       params,
       addFlashMessage
     } = this.props;
-
+    const shareUrl = 'www.facebook.com';
     if (!singleBusiness.Reviews) {
       return <h2>loading...</h2>;
     }
@@ -96,11 +109,45 @@ class SingleBusiness extends React.Component {
                 </p>
                 <p className="text-center my-4">{singleBusiness.description}</p>
                 <hr />
+                Share On:<br/> 
+                <div className="share-div">
+                    <FacebookShareButton
+                        url={shareUrl}
+                        // quote={title}
+                        className="share-btn">
+                        <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                  </div>
+                <div className="share-div">
+                  <WhatsappShareButton url={shareUrl}
+                    className="share-btn">
+                    <WhatsappIcon size={32} round />
+                  </WhatsappShareButton>
+                </div>
+                <div className="share-div">
+                  <LinkedinShareButton url={shareUrl}
+                    className="share-btn">
+                    <LinkedinIcon size={32} round />
+                  </LinkedinShareButton>
+                </div>
+                <div className="share-div">
+                  <GooglePlusShareButton url={shareUrl}
+                    className="share-btn">
+                    <GooglePlusIcon size={32} round />
+                  </GooglePlusShareButton>
+                </div>
+                <div className="share-div">
+                  <TwitterShareButton url={shareUrl}
+                    className="share-btn">
+                    <TwitterIcon size={32} round />
+                  </TwitterShareButton>
+                </div>
                 <p className="text-muted h6 text-center my-4">
                   <span className="mr-3 h3">
                     <i className="ion ion-happy-outline" />
                     {singleBusiness.Reviews.length}
                     <small>Review(s)</small>
+                    
                   </span>
                 </p>
                 <hr />

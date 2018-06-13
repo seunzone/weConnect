eed1259ef6c4132100036bb43729a26961992fb3
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-44.local>
2018-06-13 16:06:21 +0100

    remove unused lines

--- client/components/businesses/SingleBusiness.jsx
+++ client/components/businesses/SingleBusiness.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import moment from "moment";
 import rating from "../../utils/rating"

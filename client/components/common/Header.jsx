acaa794fe1f2ce586effbb946a4822c0731c1a24
seunzone <darealseun@gmail.com>
2018-07-08 13:49:42 +0100

    make home route protected for authenticated users

--- client/components/common/Header.jsx
+++ client/components/common/Header.jsx
@@ -15,50 +15,63 @@ class Header extends React.Component {
   logout(event) {
     event.preventDefault();
     this.props.logout();
-    this.context.router.history.push('/signin');
+    this.context.router.history.push("/signin");
   }
   render() {
     const { isAuthenticated } = this.props.auth;
     const userLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link to="/businesses/add" className="nav-link text-secondary">
-            Create Business
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/business" className="nav-link text-secondary">
-            See All Businesses
-          </Link>
-        </li>
-        <li className="nav-item mr-3">
-          <Link to="/dashboard" className="nav-link btn-primary text-white">
-            <i className="fa fa-tachometer" aria-hidden="true" /> My Dashboard
-          </Link>
-        </li>
-        <Link
-          className="nav-link text-white btn-danger"
-          to="/signin"
-          onClick={this.logout.bind(this)}
-        >
-          <i className="fa fa-sign-in" aria-hidden="true" /> Logout
+      <nav className="navbar navbar-expand-lg navbar-custom navbar-light">
+        <Link to="/business" className="navbar-brand" href="#">
+          <img
+            src="../../public/images/logo.png"
+            className="navbar-logo-img mr-2"
+            alt=""
+          />
         </Link>
-      </ul>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          id="toggle-me"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto" />
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/businesses/add" className="nav-link text-secondary">
+                Create Business
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/business" className="nav-link text-secondary">
+                See All Businesses
+              </Link>
+            </li>
+            <li className="nav-item mr-3">
+              <Link to="/dashboard" className="nav-link btn-primary text-white">
+                <i className="fa fa-tachometer" aria-hidden="true" /> My
+                Dashboard
+              </Link>
+            </li>
+            <Link
+              className="nav-link text-white btn-danger"
+              to="/signin"
+              onClick={this.logout.bind(this)}
+            >
+              <i className="fa fa-sign-in" aria-hidden="true" /> Logout
+            </Link>
+          </ul>
+        </div>
+      </nav>
     );
 
     const guestLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link to="/business" className="nav-link text-secondary">
-            See All Businesses
-          </Link>
-        </li>
-        <Link to="signin" className="nav-link btn-primary text-white">
-          <i className="fa fa-sign-in" aria-hidden="true" /> Sign In
-        </Link>
-      </ul>
-    );
-    return (
       <nav className="navbar navbar-expand-lg navbar-custom navbar-light">
         <Link to="/" className="navbar-brand" href="#">
           <img
@@ -81,10 +94,20 @@ class Header extends React.Component {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto" />
-          {isAuthenticated ? userLinks : guestLinks}
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/business" className="nav-link text-secondary">
+                See All Businesses
+              </Link>
+            </li>
+            <Link to="signin" className="nav-link btn-primary text-white">
+              <i className="fa fa-sign-in" aria-hidden="true" /> Sign In
+            </Link>
+          </ul>
         </div>
       </nav>
     );
+    return <div>{isAuthenticated ? userLinks : guestLinks}</div>;
   }
 }
 
